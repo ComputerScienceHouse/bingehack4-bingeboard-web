148eeafe56d65e6f9b4f1c6b8a99d1550d4ad6a2
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-03-19 04:50:22 -0400

    clear

--- js/bingeboard.js
+++ js/bingeboard.js
@@ -4,6 +4,9 @@ $(function() {
 
 function pollBingeBoard() {
     $.get('https://binge.csh.rit.edu/api/get/10?filter_complete', function(data) {
+        // Clear table rows
+        $('#bingeboard > tbody > tr').remove();
+
         for(var i = 0; i < data.length; i++) {
             var tableRow = $('#bingeboard').find('tbody').append($('<tr>'));
             tableRow.append($('<td>').text(data[i].character_name))
