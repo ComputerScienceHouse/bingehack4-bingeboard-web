d4a2b5b2facc693fbd69b31c02ee3aafe5f00eee
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-03-19 04:39:29 -0400

    add charname and score

--- js/bingeboard.js
+++ js/bingeboard.js
@@ -5,6 +5,11 @@ $(function() {
 function pollBingeBoard() {
     $.get('https://binge.csh.rit.edu/api/get/10?filter_complete', function(data) {
         console.log(data);
+        for(var i = 0; i < data.length; i++) {
+            var tableRow = $('#bingeboard').find('tbody').append($('<tr>');
+            tableRow.append($('<td>').text(data[i].character_name))
+            tableRow.append($('<td>').text(data[i].score))
+        }
     });
     setTimeout(pollBingeBoard, 5000);
 }
