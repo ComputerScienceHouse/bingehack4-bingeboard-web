49b425f6042e5e13915adfcdc05dd9cbde51dfe7
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-03-19 04:57:25 -0400

    actually clear

--- js/bingeboard.js
+++ js/bingeboard.js
@@ -11,7 +11,7 @@ $(function() {
 function pollBingeBoard() {
     $.get('https://binge.csh.rit.edu/api/get/10?filter_complete', function(data) {
         // Clear table rows
-        $('#bingeboard > tbody > tr').remove();
+        $('#bingeboard').find('tbody').empty();
 
         for(var i = 0; i < data.length; i++) {
             var tableRow = $('#bingeboard').find('tbody').append($('<tr>'));
